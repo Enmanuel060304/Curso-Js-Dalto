da95cc240f9a4edc1b8987bcfd05022898ad04de
Enmanuel Urbina <enmanuelu100@gmail.com>
2025-01-17 22:44:51 -0600

    avances

--- Cap3/coflaB.js
+++ Cap3/coflaB.js
@@ -42,16 +42,43 @@ class Celulares {
     }
 }
 
-const samsung = new Celulares("rojo", "10kg", "16 pulgadas", "4k", "4k", 16);
-samsung.prender();
-samsung.grabarVideo();
-samsung.tomarFoto();
-samsung.reiniciar();
-document.write(samsung.mobileInfo());
+// const samsung = new Celulares("rojo", "10kg", "16 pulgadas", "4k", "4k", 16);
+// samsung.prender();
+// samsung.grabarVideo();
+// samsung.tomarFoto();
+// samsung.reiniciar();
+// document.write(samsung.mobileInfo());
 
 
 //camara super lenta
 //reconomiciento facial 
 //camara extra
 
-/* */
+/* ahora implementar esta monda en celulares en gama alta*/
+
+class GamaAlta extends Celulares{
+    constructor(color, peso, tamano, rePant,reCamara, ram, redCamExt){
+        super(color, peso, tamano, rePant,reCamara, ram);
+        this.resolucionCamaraExtra = redCamExt;
+    }
+
+    TomarVideoCamaraLenta = () => {
+        alert(`se ha tomado un video en camara lente con la camara ${this.resolucionCamaraExtra}`);
+    }
+
+    ReconocimientFacial = () => {
+        alert(`telefono desbloqueado`);
+    }
+
+    InfoAltaGama = () => {
+        return this.mobileInfo() + `
+            Camara Extra: ${this.resolucionCamaraExtra} 
+        `;
+    }
+}
+
+celular1 = new GamaAlta("rojo","130g","5.2","4K","3GB","full hd");
+celular2 = new GamaAlta("negro","142g","6","4K","4GB", "hd");
+
+
+document.write(celular1.InfoAltaGama());
